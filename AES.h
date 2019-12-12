1c7ebb2fa0a7014c8b324720c2d6555290fbd2d8
Sarah Lasman <lasmansarah@gmail.com>
2019-12-11 22:35:46 -0800

    Update AES.h

--- AES.h
+++ AES.h
@@ -6,8 +6,13 @@
 #define NR 10
 #define WORDSIZE NB*(NR+1)
 
-void cipher(uint8_t* input, uint8_t* output, uint32_t word[WORDSIZE]);
-void subBytes(uint8_t* state, uint8_t* box);
+void cipher(uint8_t* input, uint8_t* output);
+void subBytes(uint8_t* state);
+void invSubBytes(uint8_t* state);
 void shiftRows(uint8_t* state, bool isInv);
-void mixColumns(uint8_t* state);
+void mixColumns(uint8_t* state, bool isInv);
 void addRoundKey(uint8_t* state, uint8_t round);
+uint8_t multGF(uint8_t a, uint8_t b);
+uint32_t subWord(uint32_t temp);
+uint32_t rotWord(uint32_t temp);
+void keyExpansion(uint8_t* key);
